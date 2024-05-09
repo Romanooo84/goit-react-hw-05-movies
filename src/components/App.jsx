3d8f4547192271f1e4447b9fb649fb5dd62a5373
Pisarski Roman <133641494+Romanooo84@users.noreply.github.com>
2024-05-09 14:24:47 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import { Trending } from "../pages/trending";
 import { Home } from "pages/home";
 
@@ -6,6 +7,11 @@ import { Home } from "pages/home";
 
 export const App = () => {
 
+  const [pictures, setPictures] = useState([])
+  
+  let onClick = (to) => {
+    console.log(to)
+  }
 
 const options = {
   method: 'GET',
@@ -13,17 +19,32 @@ const options = {
     accept: 'application/json',
     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlN2M5MzBkOWVlMjFkYTk0ZjhmYzMyNTdkMzg3ZWNlZCIsInN1YiI6IjY1ZTlhZDJhNmEyMjI3MDE4Njk2NTExNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.j1C6buHnPvlB2eInbvledvTmiFFIKSym7oMjouLo9AE'
   }
-};
+  };
+  
+  useEffect(() => {
+    fetch('https://api.themoviedb.org/3/trending/all/day?language=en-US', options)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.results)
+        const markup = data.results.map(picture =>
+          <div key={picture.id}>
+            <img src={`https://image.tmdb.org/t/p/w500/${picture.backdrop_path}`}></img>
+          </div>
+        )
+        setPictures(markup)
+      })
+      .catch(err => console.error(err));
+  },[])
   
   return (
     <div>
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/trending">Trending</Link>
+        <Link to="/" onClick={()=>onClick('/')}>Home</Link>
+        <Link to="/trending"onClick={()=>onClick('trending')}>Trending</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/trending" element={<Trending options={options} />} />
+        <Route path="/trending" element={<Trending pictures={pictures} />} />
       </Routes>
     </div>
   );
