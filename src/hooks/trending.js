c60d1014e6e31f8c20934841d66d48165f55ca73
Pisarski Roman <133641494+Romanooo84@users.noreply.github.com>
2024-05-15 08:49:35 +0000

    trending

--- src/hooks/trending.js
+++ src/hooks/trending.js
@@ -8,28 +8,29 @@ export const trending = (setMovies) => {
     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlN2M5MzBkOWVlMjFkYTk0ZjhmYzMyNTdkMzg3ZWNlZCIsInN1YiI6IjY1ZTlhZDJhNmEyMjI3MDE4Njk2NTExNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.j1C6buHnPvlB2eInbvledvTmiFFIKSym7oMjouLo9AE'
   }
   }
-    fetch('https://api.themoviedb.org/3/trending/all/day?language=en-US', options)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            const markup = data.results.map(movie => {
-                console.log(movie.original_title)
-                if ('original_title' in movie) {
-                    console.log('title')
-                    return(
-                    <li key={movie.id} id={movie.id}>
-                        <Link to={`${movie.id}`}>{movie.title}</Link>
-                    </li>)
-                }
-                else if ('original_name' in movie) {
-                  return(
-                    <li key={movie.id} id={movie.id}>
-                        <Link to={`${movie.id}`}>{movie.name}</Link>
-                    </li>)
-                }
-            }
-            )
-            setMovies(markup)
-        })
-        .catch(err => console.error(err));
+  fetch('https://api.themoviedb.org/3/trending/all/day?language=en-US', options)
+  .then(response => response.json())
+  .then(data => {
+      console.log(data);
+      const markup = data.results.map(movie => {
+          console.log(movie.original_title);
+          if ('original_title' in movie) {
+              console.log('title');
+              return (
+                  <li key={movie.id} id={movie.id}>
+                      <Link to={`${movie.id}`}>{movie.title}</Link>
+                  </li>
+              );
+          } else if ('original_name' in movie) {
+              return (
+                  <li key={movie.id} id={movie.id}>
+                      <Link to={`${movie.id}`}>{movie.name}</Link>
+                  </li>
+              );
+          }
+      });
+
+      setMovies(markup);
+  })
+  .catch(err => console.error(err));
 }
